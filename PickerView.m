90d524fb45a8707730eb8c2d00d2e1615d4b4655
鼠非鱼 <n_sfyu@3songshu.com>
2019-12-28 16:11:57 +0800

    no message

---
+++ PickerView.m
@@ -0,0 +1,445 @@
+//
+//  ZHPickView.m
+//  ZHpickView
+//
+//  Created by liudianling on 14-11-18.
+//  Copyright (c) 2014年 赵恒志. All rights reserved.
+//
+#define ZHToobarHeight 40
+#import "ZHPickView.h"
+
+@interface ZHPickView ()<UIPickerViewDelegate,UIPickerViewDataSource>
+@property(nonatomic,copy)NSString *plistName;
+@property(nonatomic,strong)NSArray *plistArray;
+@property(nonatomic,assign)BOOL isLevelArray;
+@property(nonatomic,assign)BOOL isLevelString;
+@property(nonatomic,assign)BOOL isLevelDic;
+@property(nonatomic,strong)NSDictionary *levelTwoDic;
+@property(nonatomic,strong)UIToolbar *toolbar;
+@property(nonatomic,strong)UIPickerView *pickerView;
+@property(nonatomic,assign)NSDate *defaulDate;
+@property(nonatomic,assign)BOOL isHaveNavControler;
+@property(nonatomic,assign)NSInteger pickeviewHeight;
+@property(nonatomic,copy)NSString *resultString;
+@property(nonatomic,strong)NSMutableArray *componentArray;
+@property(nonatomic,strong)NSMutableArray *dicKeyArray;
+@property(nonatomic,strong)NSMutableArray *state;
+@property(nonatomic,strong)NSMutableArray *city;
+
+@property(nonatomic,assign)NSInteger currentComponent;
+@property(nonatomic,assign)NSInteger currentRow;
+@end
+
+@implementation ZHPickView
+
+-(NSArray *)plistArray{
+    if (_plistArray==nil) {
+        _plistArray=[[NSArray alloc] init];
+    }
+    return _plistArray;
+}
+
+-(NSArray *)componentArray{
+
+    if (_componentArray==nil) {
+        _componentArray=[[NSMutableArray alloc] init];
+    }
+    return _componentArray;
+}
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setUpToolBar];
+        
+    }
+    return self;
+}
+
+
+-(instancetype)initPickviewWithPlistName:(NSString *)plistName isHaveNavControler:(BOOL)isHaveNavControler{
+    
+    self=[super init];
+    if (self) {
+        _plistName=plistName;
+        self.plistArray=[self getPlistArrayByplistName:plistName];
+        [self setUpPickView];
+        [self setFrameWith:isHaveNavControler];
+        
+    }
+    return self;
+}
+
+-(instancetype)initPickviewWithArray:(NSArray *)array isHaveNavControler:(BOOL)isHaveNavControler{
+    self=[super init];
+    if (self) {
+        self.plistArray=array;
+        [self setArrayClass:array];
+        [self setUpPickView];
+        [self setFrameWith:isHaveNavControler];
+    }
+    return self;
+}
+
+-(instancetype)initDatePickWithDate:(NSDate *)defaulDate datePickerMode:(UIDatePickerMode)datePickerMode isHaveNavControler:(BOOL)isHaveNavControler{
+    
+    self=[super init];
+    if (self) {
+        _defaulDate=defaulDate;
+        [self setUpDatePickerWithdatePickerMode:(UIDatePickerMode)datePickerMode];
+        [self setFrameWith:isHaveNavControler];
+    }
+    return self;
+}
+
+
+-(NSArray *)getPlistArrayByplistName:(NSString *)plistName{
+    
+    NSString *path= [[NSBundle mainBundle] pathForResource:plistName ofType:@"plist"];
+    NSArray * array=[[NSArray alloc] initWithContentsOfFile:path];
+    [self setArrayClass:array];
+    return array;
+}
+
+-(void)setArrayClass:(NSArray *)array{
+    _dicKeyArray=[[NSMutableArray alloc] init];
+    for (id levelTwo in array) {
+        
+        if ([levelTwo isKindOfClass:[NSArray class]]) {
+            _isLevelArray=YES;
+            _isLevelString=NO;
+            _isLevelDic=NO;
+        }else if ([levelTwo isKindOfClass:[NSString class]]){
+            _isLevelString=YES;
+            _isLevelArray=NO;
+            _isLevelDic=NO;
+            
+        }else if ([levelTwo isKindOfClass:[NSDictionary class]])
+        {
+            _isLevelDic=YES;
+            _isLevelString=NO;
+            _isLevelArray=NO;
+            _levelTwoDic=levelTwo;
+            [_dicKeyArray addObject:[_levelTwoDic allKeys] ];
+        }
+    }
+}
+
+-(void)setFrameWith:(BOOL)isHaveNavControler{
+    CGFloat toolViewX = 0;
+    CGFloat toolViewH = _pickeviewHeight+ZHToobarHeight;
+    CGFloat toolViewY ;
+    if (isHaveNavControler) {
+        toolViewY= [UIScreen mainScreen].bounds.size.height-toolViewH-50;
+    }else {
+        toolViewY= [UIScreen mainScreen].bounds.size.height-toolViewH;
+    }
+//    CGFloat toolViewW = [UIScreen mainScreen].bounds.size.width;
+//    self.frame = CGRectMake(toolViewX, toolViewY, toolViewW, toolViewH);
+    self.frame = CGRectMake(toolViewX, toolViewY, [UIScreen mainScreen].bounds.size.width, toolViewH);
+}
+
+-(void)setUpPickView{
+    
+    UIPickerView *pickView=[[UIPickerView alloc] init];
+    pickView.backgroundColor=[UIColor lightGrayColor];
+    _pickerView=pickView;
+    pickView.delegate=self;
+    pickView.dataSource=self;
+    pickView.frame=CGRectMake(0, ZHToobarHeight, [UIScreen mainScreen].bounds.size.width, pickView.frame.size.height);
+    _pickeviewHeight=pickView.frame.size.height;
+//    [self clearSeparatorWithView:_pickerView];
+    [self addSubview:pickView];
+}
+
+-(void)setUpDatePickerWithdatePickerMode:(UIDatePickerMode)datePickerMode{
+    UIDatePicker *datePicker=[[UIDatePicker alloc] init];
+    datePicker.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"];
+    datePicker.datePickerMode = datePickerMode;
+    datePicker.backgroundColor=[UIColor lightGrayColor];
+    if (_defaulDate) {
+        [datePicker setDate:_defaulDate];
+    }
+    _datePicker=datePicker;
+    datePicker.frame=CGRectMake(0, ZHToobarHeight, [UIScreen mainScreen].bounds.size.width, datePicker.frame.size.height);
+    _pickeviewHeight=datePicker.frame.size.height;
+    [self addSubview:datePicker];
+}
+
+-(void)setUpToolBar{
+    _toolbar=[self setToolbarStyle];
+    [self setToolbarWithPickViewFrame];
+    [self addSubview:_toolbar];
+}
+
+-(UIToolbar *)setToolbarStyle{
+    UIToolbar *toolbar=[[UIToolbar alloc] init];
+    toolbar.backgroundColor = [UIColor whiteColor];
+    UIBarButtonItem *lefttem=[[UIBarButtonItem alloc] initWithTitle:@"    取消" style:UIBarButtonItemStylePlain target:self action:@selector(cancelEven)];
+//    [lefttem setTitleTextAttributes:@{NSFontAttributeName:[UIFont font14],NSForegroundColorAttributeName:[UIColor color]} forState:UIControlStateNormal];
+    [lefttem setTitleTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:12],NSForegroundColorAttributeName:[UIColor orangeColor]} forState:UIControlStateNormal];
+    UIBarButtonItem *centerSpace=[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    
+    UIBarButtonItem *right=[[UIBarButtonItem alloc] initWithTitle:@"确定    " style:UIBarButtonItemStylePlain target:self action:@selector(doneClick)];
+    [right setTitleTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:12],NSForegroundColorAttributeName:[UIColor orangeColor]} forState:UIControlStateNormal];
+    toolbar.items=@[lefttem,centerSpace,right];
+    return toolbar;
+}
+
+-(void)setToolbarWithPickViewFrame{
+    _toolbar.frame=CGRectMake(0, 0,[UIScreen mainScreen].bounds.size.width, ZHToobarHeight);
+    [_toolbar setShadowImage:[UIImage new] forToolbarPosition:UIBarPositionTop];
+    _toolbar.clipsToBounds=YES;
+
+//    UILine *topLine=[[UILine alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, 0.5)];
+//    topLine.backgroundColor=[UIColor colorWithWhite:0 alpha:0.25];
+//    [_toolbar addSubview:topLine];
+//
+//     UILine *bottomLine=[[UILine alloc]initWithFrame:CGRectMake(0, ZHToobarHeight-0.5, kScreenWidth, 0.5)];
+//    [_toolbar addSubview:bottomLine];
+}
+
+#pragma mark piackView 数据源方法
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    
+    NSInteger component=0;
+    if (_isLevelArray) {
+        component=_plistArray.count;
+    }else if (_isLevelString){
+        component=1;
+    }else if(_isLevelDic){
+        component=[_levelTwoDic allKeys].count*2;
+    }
+    return component;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    NSArray *rowArray=[[NSArray alloc] init];
+    if (_isLevelArray) {
+        rowArray=_plistArray[component];
+    }else if (_isLevelString){
+        rowArray=_plistArray;
+    }else if (_isLevelDic){
+        NSInteger pIndex = [pickerView selectedRowInComponent:0];
+        NSDictionary *dic=_plistArray[pIndex];
+        for (id dicValue in [dic allValues]) {
+                if ([dicValue isKindOfClass:[NSArray class]]) {
+                    if (component%2==1) {
+                        rowArray=dicValue;
+                    }else{
+                        rowArray=_plistArray;
+                    }
+            }
+        }
+    }
+    return rowArray.count;
+}
+
+#pragma mark UIPickerViewdelegate
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    ((UILabel *)[pickerView.subviews objectAtIndex:1]).hidden = YES;//隐藏分隔线
+    ((UILabel *)[pickerView.subviews objectAtIndex:2]).hidden = YES;//隐藏分隔线
+    NSString *rowTitle=nil;
+    if (_isLevelArray) {
+        rowTitle=_plistArray[component][row];
+    }else if (_isLevelString){
+        rowTitle=_plistArray[row];
+    }else if (_isLevelDic){
+        NSInteger pIndex = [pickerView selectedRowInComponent:0];
+        NSDictionary *dic=_plistArray[pIndex];
+        if(component%2==0)
+        {
+            rowTitle=_dicKeyArray[row][component];
+        }
+        for (id aa in [dic allValues]) {
+           if ([aa isKindOfClass:[NSArray class]]&&component%2==1){
+                NSArray *bb=aa;
+                if (bb.count>row) {
+                    rowTitle=aa[row];
+                }
+                
+            }
+        }
+    }
+    return rowTitle;
+}
+
+-(NSAttributedString *)pickerView:(UIPickerView *)pickerView attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    ((UILabel *)[pickerView.subviews objectAtIndex:1]).hidden = YES;//隐藏分隔线
+    ((UILabel *)[pickerView.subviews objectAtIndex:2]).hidden = YES;//隐藏分隔线
+    NSString *rowTitle=nil;
+    if (_isLevelArray) {
+        rowTitle=_plistArray[component][row];
+    }else if (_isLevelString){
+        rowTitle=_plistArray[row];
+    }else if (_isLevelDic){
+        NSInteger pIndex = [pickerView selectedRowInComponent:0];
+        NSDictionary *dic=_plistArray[pIndex];
+        if(component%2==0)
+        {
+            rowTitle=_dicKeyArray[row][component];
+        }
+        for (id aa in [dic allValues]) {
+            if ([aa isKindOfClass:[NSArray class]]&&component%2==1){
+                NSArray *bb=aa;
+                if (bb.count>row) {
+                    rowTitle=aa[row];
+                }
+                
+            }
+        }
+    }
+    if (row==_currentRow&&component==_currentComponent) {
+        return [[NSAttributedString alloc]initWithString:rowTitle attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14],NSForegroundColorAttributeName:[UIColor lightGrayColor]}];
+    }
+    else{
+        return [[NSAttributedString alloc]initWithString:rowTitle attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:12],NSForegroundColorAttributeName:[UIColor lightGrayColor]}];
+    }
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    
+    _currentRow=row;
+    _currentComponent=component;
+    [pickerView reloadComponent:component];
+    if (_isLevelDic&&component%2==0) {
+        
+        [pickerView reloadComponent:1];
+        [pickerView selectRow:0 inComponent:1 animated:YES];
+    }
+    if (_isLevelString) {
+        _resultString=_plistArray[row];
+        
+    }else if (_isLevelArray){
+        _resultString=@"";
+        if (![self.componentArray containsObject:@(component)]) {
+            [self.componentArray addObject:@(component)];
+        }
+        for (int i=0; i<_plistArray.count;i++) {
+            if ([self.componentArray containsObject:@(i)]) {
+                NSInteger cIndex = [pickerView selectedRowInComponent:i];
+                _resultString=[NSString stringWithFormat:@"%@%@",_resultString,_plistArray[i][cIndex]];
+            }else{
+                _resultString=[NSString stringWithFormat:@"%@%@",_resultString,_plistArray[i][0]];
+                          }
+        }
+    }else if (_isLevelDic){
+        if (component==0) {
+          _state =_dicKeyArray[row][0];
+        }else{
+            NSInteger cIndex = [pickerView selectedRowInComponent:0];
+            NSDictionary *dicValueDic=_plistArray[cIndex];
+            NSArray *dicValueArray=[dicValueDic allValues][0];
+            if (dicValueArray.count>row) {
+                _city =dicValueArray[row];
+            }
+        }
+    }
+}
+
+-(void)remove{
+    [self removeFromSuperview];
+}
+
+-(void)cancelEven{
+    if ([self.delegate respondsToSelector:@selector(cancelPickView:)]) {
+        [self.delegate cancelPickView:self];
+    }
+    [self removeFromSuperview];
+}
+
+-(void)show{
+    [[UIApplication sharedApplication].keyWindow addSubview:self];
+}
+
+-(void)doneClick
+{
+    if (_pickerView) {
+        
+        if (_resultString) {
+           
+        }else{
+            if (_isLevelString) {
+                _resultString=[NSString stringWithFormat:@"%@",_plistArray[0]];
+            }else if (_isLevelArray){
+                _resultString=@"";
+                for (int i=0; i<_plistArray.count;i++) {
+                    _resultString=[NSString stringWithFormat:@"%@%@",_resultString,_plistArray[i][0]];
+                }
+            }else if (_isLevelDic){
+                
+                if (_state==nil) {
+                     _state =_dicKeyArray[0][0];
+                    NSDictionary *dicValueDic=_plistArray[0];
+                    _city=[dicValueDic allValues][0][0];
+                }
+                if (_city==nil){
+                    NSInteger cIndex = [_pickerView selectedRowInComponent:0];
+                    NSDictionary *dicValueDic=_plistArray[cIndex];
+                    _city=[dicValueDic allValues][0][0];
+                    
+                }
+              _resultString=[NSString stringWithFormat:@"%@%@",_state,_city];
+           }
+        }
+    }else if (_datePicker) {
+        _resultString=[NSString stringWithFormat:@"%@",_datePicker.date];
+    }
+    if ([self.delegate respondsToSelector:@selector(toobarDonBtnHaveClick:resultString:)]) {
+        [self.delegate toobarDonBtnHaveClick:self resultString:_resultString];
+    }
+    [self removeFromSuperview];
+}
+/**
+ *  设置PickView的颜色
+ */
+-(void)setPickViewColer:(UIColor *)color{
+    _pickerView.backgroundColor=color;
+    _datePicker.backgroundColor=color;
+}
+/**
+ *  设置toobar的文字颜色
+ */
+-(void)setTintColor:(UIColor *)color{
+    
+    _toolbar.tintColor=color;
+    _pickerView.tintColor=color;
+    _datePicker.tintColor=color;
+}
+/**
+ *  设置toobar的背景颜色
+ */
+-(void)setToolbarTintColor:(UIColor *)color{
+    
+    _toolbar.barTintColor=color;
+}
+-(void)dealloc{
+    
+    //NSLog(@"销毁了");
+}
+
+- (void)clearSeparatorWithView:(UIView * )view
+{
+    if(view.subviews != 0  )
+    {
+        if(view.bounds.size.height < 5)
+        {
+            view.backgroundColor = [UIColor clearColor];
+        }
+        
+        [view.subviews enumerateObjectsUsingBlock:^( UIView *  obj, NSUInteger idx, BOOL *  stop) {
+            [self clearSeparatorWithView:obj];
+        }];
+    }
+    
+}
+
+@end
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
+
